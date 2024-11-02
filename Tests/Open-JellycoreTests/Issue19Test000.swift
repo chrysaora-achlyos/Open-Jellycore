e246433a5b0fdab4cf667b49131f79c2e5fff311
dev <mbpro.m1.dev@ma7.org>
2024-11-02 18:52:36 -0500

    Improve test cases

--- Tests/Open-JellycoreTests/Issue19Test000.swift
+++ Tests/Open-JellycoreTests/Issue19Test000.swift
@@ -2,137 +2,158 @@ import XCTest
 @testable import Open_Jellycore
 
 final class Issue19Test: XCTestCase {
-    // Only works with magicvariagle output of dictionary(json:...) >> magicVar
-    // NO, setValue go mod dictionary
-    //    func testIssue19Test000() throws {
-    //        try execute(with: """
-    //        import Shortcuts
-    //        dictionary(json: "{}") >> h0
-    //        setValue(key: "Content-Type", value: "application/json; charset=UTF-8", dictionary: h0) >> h
-    //        var u = "http://echo.free.beeceptor.com/sample-request?author=beeceptor"
-    //        dictionary(json: "{}") >> mm0
-    //        setValue(key: "a", value: "z", dictionary: mm0) >> mm
-    //        downloadURL(url: u, method: POST, headers: h, requestType: Json, requestJSON: mm, requestVar: response) >> response
-    //        """)
-    //    }
     
-    func testIssue19Test001() throws {
-        try execute(with: """
-        import Shortcuts
-        dictionary(json: "{\\\"a\\\": \\\"b\\\"}") >> h0
-        """)
-    }
-    
-    func testIssue19Test002() throws {
+    func testIssue19Test000_Goal() throws {
         try execute(with: """
          import Shortcuts
-         dictionary(json: {"a": "b"}) >> h0
+         dictionary(json: {"Content-Type": "application/json; charset=UTF-8"}) >> header
+         dictionary(json: {}) >> unused
+         dictionary(json: {"a": "z"}) >> requestVar
+         var url = "http://echo.free.beeceptor.com/sample-request?author=beeceptor"
+         downloadURL(url: url, method: POST, headers: header, requestType: File, requestJSON: unused, requestVar: requestVar) >> response
+         quicklook(input: response)
          """)
     }
     
-    func testIssue19Test002BoolVal() throws {
+    func testIssue19Test001_legacyRegression() throws {
         try execute(with: """
          import Shortcuts
-         dictionary(json: {"a": true}) >> h0
+         dictionary(json: "{\\\"a\\\": \\\"b\\\"}") >> d0
+         quicklook(input: d0)
          """)
     }
     
+    func testIssue19Test002_curlyBrackJsonObjectLiteral() throws {
+        try execute(with: """
+         import Shortcuts
+         dictionary(json: {"a": "b"}) >> d0
+         quicklook(input: d0)
+         """)
+    }
+    
+    func testIssue19Test002a_jsonObjectLiteral_BoolVal() throws {
+        try execute(with: """
+         import Shortcuts
+         dictionary(json: {"T": true, "F": false}) >> d0
+         quicklook(input: d0)
+         """)
+    }
+    
+    // Sci notation is not preserved, converted in translation to plist
+    func testIssue19Test002a_jsonObjectLiteral_Number() throws {
+        try execute(with: """
+         import Shortcuts
+         dictionary(json: {"int": 10, "float": -10.3, "sci": -96.01E-02}) >> d0
+         quicklook(input: d0)
+         """)
+    }
+    
+    func testIssue19Test002a_jsonObjectLiteral_Array() throws {
+        try execute(with: """
+         import Shortcuts
+         dictionary(json: {"key": ["string", -10.3, true]}) >> d0
+         quicklook(input: d0)
+         """)
+    }
+    
+    func testIssue19Test002a_jsonObjectLiteral_Dictionary() throws {
+        try execute(with: """
+         import Shortcuts
+         dictionary(json: {"key": {"subKey":"subValue"}}) >> d0
+         quicklook(input: d0)
+         """)
+    }
+    
+    // works because dictionary value is constant
     func testIssue19Test003() throws {
         try execute(with: """
          import Shortcuts
-         dictionary(json: { "bar": [ "a", 1, true, false], }) >> casestudy
+         var unused = {}
+         dictionary(json: {"k": "v"}) >> compileTimeDict
+         
+         downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"Content-Type": "application/json; charset=utf-8"}, requestType: Json, requestJSON: compileTimeDict, requestVar: unused) >> response
          """)
     }
     
-    func testIssue19Test004() throws {
+    // WRONG compiles, but generates wrong results  [ ] FIX
+    func testIssue19Test003a_requstTypeJson_requestJsonRuntime() throws {
         try execute(with: """
          import Shortcuts
-         dictionary(json: { "one": 1, "tr": true}) >> casestudy
+         var unused = {}
+         dictionary(json: {"k": "v"}) >> compileTimeDict
+         setValue(key: "k", value: "newValue", dictionary: compileTimeDict) >> runTimeDict
+         downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"Content-Type": "application/json; charset=utf-8"}, requestType: Json, requestJSON: runTimeDict, requestVar: unused) >> response
+         quicklook(input: response)
          """)
     }
     
-    func testIssue19Test005() throws {
+    // PROPER use requestVar with runtime modified dictionary
+    func testIssue19Test003a_requstTypeFile_requestVarRuntime() throws {
         try execute(with: """
          import Shortcuts
-         var aa = 13
-         dictionary(json: {"k": "v"}) >> mm
-         downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"Content-Type": "application/json; charset=utf-8"}, requestType: Json, requestVar: mm) >> response
+         var unused = {}
+         dictionary(json: {"k": "v"}) >> compileTimeDict
+         setValue(key: "k", value: "newValue", dictionary: compileTimeDict) >> runTimeDict
+         downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"Content-Type": "application/json; charset=utf-8"}, requestType: File, requestJSON: unused, requestVar: runTimeDict) >> response
+         quicklook(input: response)
          """)
     }
     
-    
-    func testIssue19Test006() throws {
+    func testIssue19Test004_varCompileTimeStringJson() throws {
         try execute(with: """
          import Shortcuts
-         dictionary(json: {"k0":"v0"}) >> mm
          var aa = {"k": "v"}
-         dictionary(json: aa)
+         dictionary(json: aa) >> compileTimeString
+         quicklook(input: compileTimeString)
          """)
     }
     
-    
-    func testIssue19Test007() throws {
+    func testIssue19Test005_jsonObjectValueWithStringInterpolation() throws {
         try execute(with: """
          import Shortcuts
          var aa = "A"
-         var bb = "123${aa}456"
-         var cc = {"k":"77${aa}88"}
+         var runtimeString = {"k":"77${aa}88"}
+         quicklook(input: runtimeString)
          """)
     }
     
-    func testIssue19Test008() throws {
+    //COMPILES and RUNS, but dictionary does not have runtine string value, instead the literal ${aa} text
+    func testIssue19Test006_dictionaryFromJsonObjectValueWithStringInterpolation() throws {
         try execute(with: """
          import Shortcuts
-         var aa = 0.9
-         var bb = "123${aa}456"
-         var cc = {"k":"77${aa}88"}
+         var aa = "A"
+         var runtimeString = {"k":"77${aa}88"}
+         dictionary(json: runtimeString) >> d0
+         quicklook(input: d0)
          """)
     }
     
-    func testIssue19Test009() throws {
+    // PROPER use requestType:File, requestVar:runtimeString to handle string interpolation
+    func testIssue19Test006a_requestVarFromJsonObjectValueWithStringInterpolation() throws {
         try execute(with: """
          import Shortcuts
-         var aa = 0.9
-         var bb = "123${aa}456"
-         quicklook(input: bb)
+         dictionary(json: {"Content-Type": "application/json; charset=UTF-8"}) >> header
+         dictionary(json: {}) >> unused
+         var aa = "A"
+         var runtimeString = {"k":"77${aa}88"}
+         var url = "http://echo.free.beeceptor.com/sample-request?author=beeceptor"
+         downloadURL(url: url, method: POST, headers: header, requestType: File, requestJSON: unused, requestVar: runtimeString) >> response
+         quicklook(input: response)
          """)
     }
     
-    func testIssue19Test010() throws {
+    // INPROPER -- {"k":"77${aa}88"} -- Do NOT use requestType: Json and requestJSON: <runtimeVar>
+    func testIssue19Test006a_requestJsonFromJsonObjectValueWithStringInterpolation() throws {
         try execute(with: """
          import Shortcuts
-         var aa = 0.9
-         var bb = "123${aa}456"
-         quicklook(input: bb)
+         dictionary(json: {"Content-Type": "application/json; charset=UTF-8"}) >> header
+         dictionary(json: {}) >> unused
+         var aa = "A"
+         var runtimeString = {"k":"77${aa}88"}
+         var url = "http://echo.free.beeceptor.com/sample-request?author=beeceptor"
+         downloadURL(url: url, method: POST, headers: header, requestType: Json, requestJSON: runtimeString, requestVar: unused) >> response
+         quicklook(input: response)
          """)
-    }
-    
-    
-    func testIssue19Test011() throws {
-        try execute(with: """
-         import Shortcuts
-         dictionary(json: {"k0":0})
-         """)
-    }
-    
-    
-    func testIssue19Test012_literal() throws {
-        try execute(with: """
-         import Shortcuts
-         downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"Content-Type": "application/json; charset=utf-8"}, requestType: Json, requestJSON: {"k":"v"}, requestVar: response) >> response
-         """)
-    }
-    
-
-
-    
-    func testIssue19Test012_fileVar() throws {
-        try execute(with: """
-           import Shortcuts
-           var num = 0.9
-           var json = { "title": "My First Post", "body": "0.7"}
-           downloadURL( url: "http://echo.free.beeceptor.com/sample-request?author=beeceptor", method: POST, headers: {"XXContent-Type": "application/json; charset=utf-8"}, requestType: File, requestVar: json) >> response
-           """)
     }
 }
 
